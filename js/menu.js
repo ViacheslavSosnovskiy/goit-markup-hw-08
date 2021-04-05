8be8f495cadb06e7407267838bfd348285ec50e4
ViacheslavSosnovskiy <saintstill333@gmail.com>
2021-04-05 22:20:38 +0300

    delete test

--- js/menu.js
+++
@@ -1,9 +0,0 @@
-(() => {
-    const menuBtnRef = docoment.querySelector("[data-menu-button]");
-
-    menuBtnRef .addEventListener("click", () => {
-
-        menyBtnRef.classList.toggle("is-open");
-    
-});
-})();
